d43632c9c210133dfcb2bb3deb99c4d4af299f8f
gconbat7 <gildocode7@gmail.com>
2024-05-19 23:02:47 -0300

    Estilizando a tela de cadastro

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import GlobalStyle from './globalStyle.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <>
+        <App /> <GlobalStyle />
+    </>,
+    document.getElementById('root')
+);
